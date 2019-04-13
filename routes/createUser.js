1e914a922f4b82c80df5003531f175be4bf1205a
amine <amine.hamdi@tech-instinct.com>
2019-04-13 10:59:11 +0100

    check that username is not null

--- routes/createUser.js
+++ routes/createUser.js
@@ -2,15 +2,19 @@ const { resolve } = require("path");
 const { addUser, getUserByUserName } = require(resolve("db/userDao"));
 module.exports.createUser = async (req, res) => {
   const username = req.body.username;
-  const user = await getUserByUserName(username);
-  if (user === "User not found") {
-    let addUserResult = await addUser(username);
-    if (addUserResult != "Error") {
-      res.status(200).json(addUserResult);
+  if (username != "") {
+    const user = await getUserByUserName(username);
+    if (user === "User not found") {
+      let addUserResult = await addUser(username);
+      if (addUserResult != "Error") {
+        res.status(200).json(addUserResult);
+      } else {
+        res.status(500).json({ message: "Error to add user" });
+      }
     } else {
-      res.status(500).json({ message: "Error to add user" });
+      res.send("username already taken");
     }
   } else {
-    res.send("username already taken");
+    res.send("Path `username` is required.");
   }
 };
