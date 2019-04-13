599081291aa35337df7a901cd8343649b694d4c5
amine <amine.hamdi@tech-instinct.com>
2019-04-13 16:06:50 +0100

    add updateUser to userDao

--- db/userDao.js
+++ db/userDao.js
@@ -1,7 +1,11 @@
 var mongoose = require("mongoose");
 const { getDbConnection } = require("./connection");
 const shortid = require("shortid");
-const User = mongoose.model("User", { _id: String, username: String });
+const User = mongoose.model("User", {
+  _id: String,
+  username: String,
+  log: Array
+});
 const logger = require("loggy");
 const getUserByUserName = async username => {
   let result;
@@ -21,11 +25,29 @@ const getUserByUserName = async username => {
   return result[0];
 };
 
+const getUserById = async id => {
+  let result;
+  await getDbConnection();
+  await User.find({ _id: id }, (err, data) => {
+    if (err) {
+      logger.warn(`************ error in getUserById ${e}`);
+      return "Error";
+    } else {
+      result = data;
+    }
+  });
+  if (result.length == 0) {
+    return "User not found";
+  }
+
+  return result[0];
+};
+
 const addUser = async username => {
   try {
     await getDbConnection();
     logger.log(`************ connected`);
-    const userToAdd = { username: username, _id: shortid.generate() };
+    const userToAdd = { username: username, _id: shortid.generate(), log: [] };
     const shortUrlToAdd = new User(userToAdd);
     await shortUrlToAdd.save();
     logger.log(`************ user saved,${JSON.stringify(userToAdd)}`);
@@ -35,4 +57,14 @@ const addUser = async username => {
     return "Error";
   }
 };
-module.exports = { addUser, getUserByUserName };
+
+const updateUser = async (_id, log) => {
+  await User.updateOne({ _id: _id }, { log: log }, (err, user) => {
+    if (err) {
+      logger.warn(`Error when updateUser ${err}`);
+    } else {
+      logger.info(`user updated ${JSON.stringify(user)}`);
+    }
+  });
+};
+module.exports = { addUser, getUserByUserName, getUserById, updateUser };
